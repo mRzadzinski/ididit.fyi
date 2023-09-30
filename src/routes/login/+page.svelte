c200d29cc27b1dbad6b08e3ff9c943fd13c514cb
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-09-30 18:19:01 +0200

    Deal with TS error when importing fontAwesome icon

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,26 +1,27 @@
-<script>
-
+<script lang="ts">
+	import Fa from "/Users/Coding/repos/ididit.fyi/node_modules/svelte-fa/src/fa.svelte";
+	import { faGoogle } from '/Users/Coding/repos/ididit.fyi/node_modules/@fortawesome/free-brands-svg-icons/index.mjs';
 </script>
 
 <div class="prose prose-sm card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-    <div class="card-body">
-      <div class="form-control">
-        <h1>Login</h1>
-        <label for="email-input" class="label">
-          <span class="label-text">Email</span>
-        </label>
-        <input id="email-input" type="text" placeholder="email" class="input input-bordered" />
-      </div>
-      <div class="form-control">
-        <label for="password-input" class="label">
-          <span class="label-text">Password</span>
-        </label>
-        <input id="password-input" type="text" placeholder="password" class="input input-bordered" />
-        <a href="#" class="label-text-alt link link-hover mt-2">Forgot password?</a>
-      </div>
-      <div class="form-control mt-6">
-        <button class="btn btn-primary w-36">Login</button>
-      </div>
-      
-    </div>
-  </div>
+	<div class="card-body">
+		<div class="form-control">
+			<h1>Login</h1>
+			<label for="email-input" class="label">
+				<span class="label-text">Email</span>
+			</label>
+			<input id="email-input" type="text" placeholder="email" class="input input-bordered" />
+		</div>
+		<div class="form-control">
+			<label for="password-input" class="label">
+				<span class="label-text">Password</span>
+			</label>
+			<input id="password-input" type="text" placeholder="password" class="input input-bordered" />
+			<a href="#" class="label-text-alt link link-hover mt-2">Forgot password?</a>
+		</div>
+		<div class="form-control mt-6">
+			<button class="btn btn-primary w-36">Login</button>
+		</div>
+		<Fa icon={faGoogle} size='2x'/>
+	</div>
+</div>
