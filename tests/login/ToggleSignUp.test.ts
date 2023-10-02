2a1cf86009f1b4092ca7fb7ac2b186f3a45a4bed
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-02 18:57:50 +0200

    Test ToggleSignUp component

---
+++ tests/login/ToggleSignUp.test.ts
@@ -0,0 +1,47 @@
+import ToggleSignUp__SvelteComponent_ from '../../src/routes/components/login/ToggleSignUp.svelte';
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+describe('Test ToggleSignUp component', () => {
+	it('Render button', async () => {
+		render(ToggleSignUp__SvelteComponent_, { signUp: false, toggleSignUp: () => {} });
+
+		const btn = screen.getByRole('button');
+
+		expect(btn).toBeInTheDocument();
+	});
+
+	it('Call toggleSignUp function on button click', async () => {
+		const user = userEvent.setup();
+		const toggleSignUp = vi.fn(() => {});
+
+		render(ToggleSignUp__SvelteComponent_, { signUp: true, toggleSignUp: toggleSignUp });
+
+		const btn = screen.getByRole('button');
+		await user.click(btn);
+		await user.click(btn);
+		await user.click(btn);
+
+		expect(toggleSignUp).toHaveBeenCalledTimes(3);
+	});
+
+	it('Display appropriate text', async () => {
+		const { rerender } = render(ToggleSignUp__SvelteComponent_, {
+			signUp: false,
+			toggleSignUp: () => {}
+		});
+
+		let btn = screen.getByRole('button');
+		expect(btn).toHaveTextContent(/Need account?/i);
+
+		rerender({
+			signUp: true,
+			toggleSignUp: () => {}
+		});
+
+		btn = screen.getByRole('button');
+		expect(btn).toHaveTextContent(/Go to login/i);
+	});
+});
