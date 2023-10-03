6818d7a5a78ff66cd319fd8c99d6661417a66d38
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-03 13:53:37 +0200

    Add Microsoft to LoginProviders

--- src/routes/components/login/LoginProviders.svelte
+++ src/routes/components/login/LoginProviders.svelte
@@ -2,10 +2,20 @@
 	import Google from '~icons/bi/google';
 	import Apple from '~icons/bi/apple';
 	import Facebook from '~icons/bi/facebook';
+	import Microsoft from '~icons/bi/microsoft';
 </script>
 
 <div class="flex justify-center gap-5 w-full">
-	<Facebook style="font-size: 2rem; color: #617188; cursor: pointer;" />
-	<Apple style="font-size: 2.1rem; color: #617188; margin-top: -.2rem; cursor: pointer;" />
-	<Google style="font-size: 2rem; color: #617188; cursor: pointer;" />
+	<div class="tooltip tooltip-bottom" data-tip="Apple">
+		<Apple style="font-size: 2.1rem; color: #617188; margin-top: -.2rem; cursor: pointer;" />
+	</div>
+	<div class="tooltip tooltip-bottom" data-tip="Microsoft">
+		<Microsoft style="font-size: 1.85rem; color: #617188; cursor: pointer;" />
+	</div>
+	<div class="tooltip tooltip-bottom" data-tip="Google">
+		<Google style="font-size: 1.9rem; color: #617188; cursor: pointer;" />
+	</div>
+	<div class="tooltip tooltip-bottom" data-tip="Facebook">
+		<Facebook style="font-size: 2rem; color: #617188; cursor: pointer; margin-top: -.1rem;" />
+	</div>
 </div>
