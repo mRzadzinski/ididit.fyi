bbb7366349d8da524b7b33453cf063332097cd51
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-03 14:23:44 +0200

    Use tailwind palette on LoginProviders icons

--- src/routes/components/login/LoginProviders.svelte
+++ src/routes/components/login/LoginProviders.svelte
@@ -3,19 +3,21 @@
 	import Apple from '~icons/bi/apple';
 	import Facebook from '~icons/bi/facebook';
 	import Microsoft from '~icons/bi/microsoft';
+
+	const iconsColor = 'hsl(var(--bc)/.8)';
 </script>
 
 <div class="flex justify-center gap-5 w-full">
 	<div class="tooltip tooltip-bottom" data-tip="Apple">
-		<Apple style="font-size: 2.1rem; color: #617188; margin-top: -.2rem; cursor: pointer;" />
+		<Apple style="font-size: 2.1rem; color: {iconsColor}; margin-top: -.2rem; cursor: pointer; opacity: .8" />
 	</div>
 	<div class="tooltip tooltip-bottom" data-tip="Microsoft">
-		<Microsoft style="font-size: 1.85rem; color: #617188; cursor: pointer;" />
+		<Microsoft style="font-size: 1.85rem; color: {iconsColor}; cursor: pointer;" />
 	</div>
 	<div class="tooltip tooltip-bottom" data-tip="Google">
-		<Google style="font-size: 1.9rem; color: #617188; cursor: pointer;" />
+		<Google style="font-size: 1.9rem; color: {iconsColor}; cursor: pointer;" />
 	</div>
 	<div class="tooltip tooltip-bottom" data-tip="Facebook">
-		<Facebook style="font-size: 2rem; color: #617188; cursor: pointer; margin-top: -.1rem;" />
+		<Facebook style="font-size: 2rem; color: {iconsColor}; cursor: pointer; margin-top: -.1rem;" />
 	</div>
 </div>
