2bbad136e77fd7c92c85d5fd40cf1ea97f1f2b54
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-09 13:32:32 +0200

    Handle google sign in error

--- src/routes/auth/sign-in-with-google/+page.svelte
+++ src/routes/auth/sign-in-with-google/+page.svelte
@@ -2,6 +2,8 @@
 	import { firstSignIn, user } from '$lib/stores/firebaseStores';
 	import { checkGoogleSignInStatus, signInWithGoogle } from '$lib/firebase/auth/googleAuth';
 	import { goto } from '$app/navigation';
+	import { signInError } from '$lib/stores/firebaseErrors';
+	import ErrorMessage from '../../../components/ErrorMessage.svelte';
 
 	$: if ($user === null) {
 		signInWithGoogle();
@@ -16,4 +18,24 @@
 	}
 </script>
 
-<h1>hello hello google</h1>
+{#if $user === null}
+	<h1>Waiting for Google...</h1>
+	<div class="w-full flex justify-center">
+		<span class="loading loading-spinner loading-lg" />
+	</div>
+{:else if $user}
+	<div class="w-full flex justify-center">
+		<span class="loading loading-spinner loading-lg" />
+	</div>
+{:else if $signInError !== ''}
+	<ErrorMessage message={`Google sign in failed: ${$signInError}. <br>Go back to sign in page.`} />
+	<button
+		on:click={() => {
+			goto('/auth');
+			signInError.set('');
+		}}
+		class="btn btn-active btn-neutral"
+	>
+		Sign in page
+	</button>
+{/if}
