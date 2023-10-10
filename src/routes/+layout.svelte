280e2012fd238bc2b18b9158090ec76f9b4878cf
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-10 14:03:19 +0200

    Add app loading screen in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import { firebaseApp } from '$lib/firebase/firebase';
+	import { user } from '$lib/stores/firebaseStores';
 	import '../app.css';
 </script>
 
+{#if $user === undefined}
+	<div class="w-screen h-screen flex justify-center items-center flex-col gap-3 text-xl">
+		<div>
+			<h1>ididit.fyi</h1>
+		</div>
+		<span class="loading loading-spinner loading-lg" />
+	</div>
+{/if}
 <slot />
