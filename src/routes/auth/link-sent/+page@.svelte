e54900bdaedb658b3a79a99ddc9342e984fcbcdc
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-11 16:04:09 +0200

    Correct styling in link-sent page

--- src/routes/auth/link-sent/+page@.svelte
+++ src/routes/auth/link-sent/+page@.svelte
@@ -1,4 +1,4 @@
-<main class="prose flex h-screen w-screen justify-center items-center flex-col max-w-none">
+<main class="prose flex h-screen w-screen justify-center items-center flex-col max-w-none p-4">
 	<h1 class="mb-6">Check your email</h1>
 	<h2 class="mt-0">The link should be there</h2>
 	<h4>It usually arrives immediately, but sometimes it may take several minutes.</h4>
