e4e20a0cc77f89999fc440dabb4d6b771c4d01c4
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-10-11 15:50:16 +0200

    Correct link-sent page text content

--- src/routes/auth/link-sent/+page@.svelte
+++ src/routes/auth/link-sent/+page@.svelte
@@ -1,7 +1,5 @@
-<main
-	class="prose flex h-screen w-screen justify-center items-center flex-col max-w-none"
->
+<main class="prose flex h-screen w-screen justify-center items-center flex-col max-w-none">
 	<h1 class="mb-6">Check your email</h1>
-    <h2 class="mt-0">the link should be there</h2>
-    <h4>it usually arrives immediately, but sometimes it may take several minutes.</h4>
+	<h2 class="mt-0">The link should be there</h2>
+	<h4>It usually arrives immediately, but sometimes it may take several minutes.</h4>
 </main>
