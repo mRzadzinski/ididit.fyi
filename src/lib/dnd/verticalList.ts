4002662cc1b3bd3f24f64489b4be618f0186ccd9
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-11-09 13:42:43 +0100

    Extract drag&drop logic to lib module

---
+++ src/lib/dnd/verticalList.ts
@@ -0,0 +1,96 @@
+import Muuri from 'muuri';
+
+export function initializeDnd(listContainer: HTMLElement) {
+	// Initialize drag & drop
+	const grid = new Muuri(listContainer, {
+		dragEnabled: true,
+		dragAxis: 'y',
+		dragStartPredicate: { distance: 1 },
+		dragSortHeuristics: {
+			sortInterval: 0,
+			minDragDistance: 0
+		},
+		dragSortPredicate: {
+			threshold: 30
+		},
+		itemDraggingClass: 'drag-item'
+	});
+	// Clear grid if not empty
+	grid.remove(grid.getItems());
+
+	return grid;
+}
+
+export function addItemDnd(
+	listContainer: HTMLElement,
+	dndGrid: Muuri,
+	dndItems: (Element | null)[],
+	dndInitialListFill: boolean
+) {
+	const updatedDndItems = [...dndItems];
+	let initialListFill = dndInitialListFill;
+
+	// Add item to muuri if missing
+	for (let i = 0; i < listContainer.children.length; i++) {
+		const el = listContainer.children.item(i);
+		if (!updatedDndItems.includes(el) && el !== null) {
+			const htmlEl = el as HTMLElement;
+			// Add items as hidden during initial fill (bulk adding doesn't look good)
+			if (initialListFill) {
+				dndGrid.add(htmlEl, { active: false });
+			} else {
+				dndGrid.add(htmlEl);
+			}
+			updatedDndItems.push(el);
+		}
+	}
+	// Show list after initial fill
+	if (initialListFill) {
+		dndGrid.show(dndGrid.getItems());
+		initialListFill = false;
+	}
+
+	return {
+		updatedDndItems,
+		initialListFill
+	};
+}
+
+export function removeItemDnd(
+	listContainer: HTMLElement,
+	dndGrid: Muuri,
+	dndItems: (Element | null)[]
+) {
+	let updatedDndItems = [...dndItems];
+
+	// Remove muuri item if not in listContainer HTMLcollection
+	updatedDndItems.forEach((muuriItem) => {
+		let found = false;
+		for (let i = 0; i < listContainer.children.length; i++) {
+			if (listContainer.children.item(i) === muuriItem) {
+				found = true;
+			}
+		}
+		if (!found) {
+			dndGrid.remove(dndGrid.getItems(muuriItem as HTMLElement));
+			updatedDndItems = updatedDndItems.filter((item) => item !== muuriItem);
+		}
+	});
+	return updatedDndItems;
+}
+
+export function sortListDnd(dndGrid: Muuri) {
+	// Sort dnd list by item order
+	dndGrid.sort(function (itemA, itemB) {
+		const elA = itemA.getElement();
+		const elB = itemB.getElement();
+		const orderAttrA = elA ? elA.getAttribute('data-order') : null;
+		const orderAttrB = elB ? elB.getAttribute('data-order') : null;
+
+		if (orderAttrA && orderAttrB) {
+			return parseInt(orderAttrA) - parseInt(orderAttrB);
+		} else {
+			return 0;
+		}
+	});
+}
--- src/routes/app/seeds/+page.svelte
+++ src/routes/app/seeds/+page.svelte
@@ -2,104 +2,49 @@
 	import SeedsDeck from '$components/seeds/SeedsDeck.svelte';
 	import { seedsData } from '$lib/stores/dbStores';
 	import { afterUpdate, onDestroy, onMount } from 'svelte';
-	import Muuri from 'muuri';
+	import type Muuri from 'muuri';
+	import { initializeDnd, addItemDnd, removeItemDnd, sortListDnd } from '$lib/dnd/verticalList';
 
 	let listContainer: HTMLElement;
-	let grid: Muuri;
-	let muuriItems: (Element | null)[] = [];
-	let seedListVisibilityTimeout: NodeJS.Timeout;
-	let initialListFill = true;
+	let dndList: Muuri;
+	let dndItems: (Element | null)[] = [];
+	let dndInitialListFill = true;
 	let draggedItemID: string;
 	let initialPosition: number;
 	let droppedPosition: number;
 
 	onMount(() => {
 		// Initialize drag & drop
-		grid = new Muuri(listContainer, {
-			dragEnabled: true,
-			dragAxis: 'y',
-			dragStartPredicate: { distance: 1 },
-			dragSortHeuristics: {
-				sortInterval: 0,
-				minDragDistance: 0
-			},
-			dragSortPredicate: {
-				threshold: 30
-			},
-			itemDraggingClass: 'drag-item'
-		});
-		// Clear grid if not empty
-		grid.remove(grid.getItems());
+		dndList = initializeDnd(listContainer);
 
 		// Grid events
-		grid.on('dragInit', function (item, event) {
+		dndList.on('dragInit', function (item, event) {
 			const itemEl = item.getElement();
 			if (itemEl) {
 				draggedItemID = itemEl.id;
-				initialPosition = grid.getItems().indexOf(item);
+				initialPosition = dndList.getItems().indexOf(item);
+				console.log(initialPosition);
 			}
-			console.log(initialPosition);
 		});
-		grid.on('dragEnd', function (item, event) {
-			droppedPosition = grid.getItems().indexOf(item);
+		dndList.on('dragEnd', function (item, event) {
+			droppedPosition = dndList.getItems().indexOf(item);
 			console.log(droppedPosition);
 		});
 	});
 
 	afterUpdate(async () => {
-		// Add item to muuri if missing
-		for (let i = 0; i < listContainer.children.length; i++) {
-			const el = listContainer.children.item(i);
-			if (!muuriItems.includes(el) && el !== null) {
-				const htmlEl = el as HTMLElement;
-				// Add items as hidden during initial fill (bulk adding doesn't look good)
-				if (initialListFill) {
-					grid.add(htmlEl, { active: false });
-				} else {
-					grid.add(htmlEl);
-				}
-				muuriItems.push(el);
-			}
-		}
-		// Show list after initial fill
-		if (initialListFill) {
-			grid.show(grid.getItems());
-			initialListFill = false;
-		}
-
-		// Remove muuri item if not in listContainer HTMLcollection
-		muuriItems.forEach((muuriItem) => {
-			let found = false;
-			for (let i = 0; i < listContainer.children.length; i++) {
-				if (listContainer.children.item(i) === muuriItem) {
-					found = true;
-				}
-			}
-			if (!found) {
-				grid.remove(grid.getItems(muuriItem as HTMLElement));
-				muuriItems = muuriItems.filter((item) => item !== muuriItem);
-			}
-		});
-		// Sort dnd list by item order
-		grid.sort(function (itemA, itemB) {
-			const elA = itemA.getElement();
-			const elB = itemB.getElement();
-			let orderAttrA = elA ? elA.getAttribute('data-order') : null;
-			let orderAttrB = elB ? elB.getAttribute('data-order') : null;
-
-			if (orderAttrA && orderAttrB) {
-				return parseInt(orderAttrA) - parseInt(orderAttrB);
-			} else {
-				return 0;
-			}
-		});
+		// Add item to dnd list
+		const addItemInfo = addItemDnd(listContainer, dndList, dndItems, dndInitialListFill);
+		dndItems = addItemInfo.updatedDndItems;
+		dndInitialListFill = addItemInfo.initialListFill;
+		// Remove item from dnd list
+		dndItems = removeItemDnd(listContainer, dndList, dndItems);
+		// Sort dnd list
+		sortListDnd(dndList);
 	});
 
 	onDestroy(() => {
-		if (seedListVisibilityTimeout) {
-			clearTimeout(seedListVisibilityTimeout);
-		}
-		grid.destroy();
+		dndList.destroy();
 	});
 </script>
 
