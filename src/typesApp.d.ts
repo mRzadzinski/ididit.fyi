a4011929692ad1315a7785d216de39af62472e15
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-12-16 17:06:50 +0100

    Separate svelte types from app types

---
+++ src/typesApp.d.ts
@@ -0,0 +1,51 @@
+import type { Timestamp } from 'firebase/firestore';
+import type { DocumentData } from 'firebase/firestore';
+
+declare global {
+	interface UserDoc {
+		remainingSpace: number;
+		doc: DocumentData;
+		docID: string;
+	}
+
+	interface SeedType {
+		id: string;
+		date: Timestamp;
+		content: string;
+		author: string;
+		source: string;
+		showEveryday: boolean;
+	}
+
+	interface SeedsDeckType {
+		id: string;
+		name: string;
+		dailyLimit: number;
+		order: number;
+		seeds: SeedType[];
+	}
+
+	interface Subscription {
+		status: boolean;
+		counters: {
+			img: number;
+			goals: number;
+			seeds: number;
+			vision: number;
+			dreams: number;
+			wishlist: number;
+			books: number;
+		};
+	}
+
+	interface Settings {
+		decksOrderBy: string;
+		seedsOrderBy: string;
+	}
+
+	interface SortDndData {
+		id: string;
+		data: number | string;
+	}
+}
+export { SeedType, SeedsDeckType };
