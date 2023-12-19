3b3d80d6a71f307e98c36dbc2979885868c2e6bb
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-12-19 13:48:43 +0100

    Hide auth layout when loading logging in

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,4 +1,11 @@
-<main class="flex w-full h-screen justify-center items-center flex-col">
+<script>
+	import { user } from '$lib/stores/authStores';
+</script>
+
+<main
+	class="flex w-full h-screen justify-center items-center flex-col
+	{$user === null ? '' : 'invisible'}"
+>
 	<div class="prose prose-sm card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
 		<div class="card-body min-w-[320px] h-[35rem] rounded-2xl">
 			<slot />
