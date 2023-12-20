cedb83602e88f6754acd26bff3bd034f1f35a1bc
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-12-20 19:31:44 +0100

    Write ReviewContentSeed component

---
+++ src/components/daily-review/ReviewContentSeed.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let seed: SeedType;
+</script>
+
+<div class="text-xl w-full">{seed.content}</div>
+<div class="text-base opacity-60">
+	{#if seed.author}
+		<br />
+		<span>{seed.author}</span>
+	{/if}
+	{#if seed.source}
+		<br />
+		<span class="italic">{seed.source}</span>
+	{/if}
+</div>
--- src/components/daily-review/ReviewInterface.svelte
+++ src/components/daily-review/ReviewInterface.svelte
@@ -2,23 +2,36 @@
 	import CloseReview from '../common/ButtonClose.svelte';
 	import ReviewModal from './ReviewModal.svelte';
 	import ButtonArrowLeft from '../common/ButtonArrowLeft.svelte';
+	import ReviewContentSeed from './ReviewContentSeed.svelte';
+	import { seedsData } from '$lib/stores/dbStores';
 
 	export let closeReview: () => void;
 </script>
 
 <ReviewModal>
 	<div class="flex flex-col max-w-screen-xl w-full h-full">
+		<!-- Upper part -->
 		<progress class="progress w-full" value="70" max="100" />
 		<div class="self-end mr-4 mt-4">
 			<CloseReview handler={closeReview} />
 		</div>
+
+		<!-- Content -->
 		<div class="grow flex justify-between items-center">
-			<div class="ml-4"><ButtonArrowLeft handler={() => {}} /></div>
-			content
+			<div class="ml-4">
+				<ButtonArrowLeft handler={() => {}} />
+			</div>
+			<div class="px-16">
+				<ReviewContentSeed seed={$seedsData.decks[0].seeds[1]} />
+			</div>
 			<!-- Rotate 180 deg to get arrow right -->
-			<div class="mr-4 rotate-180"><ButtonArrowLeft handler={() => {}} /></div>
+			<div class="mr-4 rotate-180">
+				<ButtonArrowLeft handler={() => {}} />
+			</div>
 		</div>
-		<div class="flex justify-center items-center gap-4 h-20">
+
+		<!-- Instructions -->
+		<div class="flex justify-center items-center gap-4 h-24">
 			<span>Use</span>
 			<div class="flex justify-center gap-1">
 				<kbd class="kbd">◀︎</kbd>
