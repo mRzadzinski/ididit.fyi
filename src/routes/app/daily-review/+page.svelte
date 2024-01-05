70bfb06245cd301dcb005d020e2c437664321410
Maciej Rzadzinski <maciej.rzd@gmail.com>
2024-01-05 14:48:35 +0100

    Add review more button

--- src/routes/app/daily-review/+page.svelte
+++ src/routes/app/daily-review/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import ReviewInterface from '$components/daily-review/ReviewInterface.svelte';
 	import { getReview } from '$lib/app-logic/reviewLogic';
+	import { auth } from '$lib/firebase/firebase';
 	import { seedsData, settings } from '$lib/stores/dbStores';
 
 	let appIsEmpty = true;
 	let showReview = false;
 
 	// Check if there is anything to review
-	for (let i = 0 ; i < $seedsData.decks.length; i++) {
+	for (let i = 0; i < $seedsData.decks.length; i++) {
 		if ($seedsData.decks[i].seeds.length > 0) {
 			appIsEmpty = false;
 		}
@@ -38,18 +39,19 @@
 	</div>
 	<div class="flex justify-center items-center mt-[20%]">
 		{#if appIsEmpty}
-		<div class="text-center text-xl">
-			There is nothing to review.
-		</div>
+			<div class="text-center text-xl">There is nothing to review.</div>
 		{:else if !$settings.dailyReviewInfo.done}
 			<button class="btn btn-lg w-48 bg-[#FFCD4C]" on:click={() => (showReview = true)}>
 				START
 			</button>
 		{:else}
-			<div class="text-center text-xl">
-				Review is done.
-				<br />
-				See you tomorrow!
+			<div class="flex flex-col justify-center gap-4">
+				<div class="text-center text-xl">
+					Review is done.
+					<br />
+					See you tomorrow!
+				</div>
+				<button class="btn btn-sm opacity-40" on:click={getReview}>Review more</button>
 			</div>
 		{/if}
 	</div>
