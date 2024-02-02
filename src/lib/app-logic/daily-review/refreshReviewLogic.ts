3bcdc61dc6c5646aaa08b419c67b6ced92039af4
Maciej Rzadzinski <maciej.rzd@gmail.com>
2024-02-02 14:13:15 +0100

    Minor fixes in refreshReviewLogic

--- src/lib/app-logic/daily-review/refreshReviewLogic.ts
+++ src/lib/app-logic/daily-review/refreshReviewLogic.ts
@@ -1,7 +1,7 @@
 import { db } from '$lib/firebase/firebase';
 import { seedsData, syncInProgress, userDocs } from '$lib/stores/dbStores';
 import { collection, deleteField, doc, writeBatch } from 'firebase/firestore';
-import { cloneDeep, isEqual, uniq } from 'lodash';
+import { cloneDeep, isEqual } from 'lodash';
 import { get } from 'svelte/store';
 import type { DailyReviewDB } from './reviewLogic';
 import sizeof from 'firestore-size';
@@ -28,6 +28,7 @@ export async function refreshReview() {
 			if (!isEqual(newReview, oldReview)) {
 				// Manage remaining space in doc
 				if (
+					// Parent doc can't fit the update
 					newReviewSize > oldReviewSize &&
 					usrDocs[i].remainingSpace + oldReviewSize - newReviewSize < 0
 				) {
@@ -56,7 +57,9 @@ export async function refreshReview() {
 						const newDocRef = doc(collection(db, 'users'));
 						batch.set(newDocRef, docObj);
 					}
-				} else {
+				}
+				// Parent doc can fit the update
+				else {
 					batch.update(docRef, { dailyReview: newReview });
 				}
 			}
@@ -82,26 +85,26 @@ function clearRemovedDecksAndSeeds(review: DailyReviewDB) {
 		const reviewDeck = review.decks[i];
 		const seeds = review.decks[i].seeds;
 		const reviewed = review.decks[i].reviewed;
-		const appDeck = appData.decks.filter((deck) => deck.id === reviewDeck.id)[0];
+		const appDeck = appData.decks.find((deck) => deck.id === reviewDeck.id);
 
 		if (appDeck) {
 			// Clear nonexistent seeds
 			for (let j = 0; j < seeds.length; j++) {
-				const filtered = appDeck.seeds.filter((sd) => sd.id === seeds[j]);
-				const seedExists = filtered.length > 0 ? true : false;
-				if (!seedExists) {
+				const seed = appDeck.seeds.find((sd) => sd.id === seeds[j]);
+				if (!seed) {
 					seeds.splice(j, 1);
 				}
 			}
 			// Clear nonexistent reviewed seeds
 			for (let j = 0; j < reviewed.length; j++) {
-				const filtered = appDeck.seeds.filter((sd) => sd.id === reviewed[j]);
-				const seedExists = filtered.length > 0 ? true : false;
-				if (!seedExists) {
+				const seed = appDeck.seeds.find((sd) => sd.id === reviewed[j]);
+				if (!seed) {
 					reviewed.splice(j, 1);
 				}
 			}
-		} else {
+		}
+		// Remove nonexistent deck
+		else {
 			review.decks.splice(i, 1);
 		}
 	}
@@ -119,9 +122,9 @@ function updateReviewDeckLimits(review: DailyReviewDB) {
 
 	for (let i = 0; i < review.decks.length; i++) {
 		const reviewDeck = review.decks[i];
-		const appDeck = appData.decks.filter((deck) => deck.id === reviewDeck.id)[0];
+		const appDeck = appData.decks.find((deck) => deck.id === reviewDeck.id);
 
-		if (appDeck.dailyLimit !== reviewDeck.dailyLimit) {
+		if (appDeck && appDeck.dailyLimit !== reviewDeck.dailyLimit) {
 			reviewDeck.dailyLimit = appDeck.dailyLimit;
 		}
 	}
@@ -148,74 +151,75 @@ function adjustDeckReviewsCount(review: DailyReviewDB) {
 	// Add / remove seeds
 	for (let i = 0; i < review.decks.length; i++) {
 		const reviewDeck = review.decks[i];
-		const appDeck = appData.decks.filter((deck) => deck.id === reviewDeck.id)[0];
+		const appDeck = appData.decks.find((deck) => deck.id === reviewDeck.id);
 		const limit = reviewDeck.dailyLimit;
 		const reviewedCount = reviewDeck.reviewedCount;
 		const everydaySeeds: string[] = [];
 
 		// Filter available seeds (exclude reviewed and already in review)
-		let seedsPool: SeedType[] | string[] = appDeck.seeds.filter((seed) => {
-			for (let j = 0; j < reviewDeck.reviewed.length; j++) {
-				if (reviewDeck.reviewed[j] === seed.id) {
-					return false;
+		if (appDeck) {
+			let seedsPool: SeedType[] | string[] = appDeck.seeds.filter((seed) => {
+				for (let j = 0; j < reviewDeck.reviewed.length; j++) {
+					if (reviewDeck.reviewed[j] === seed.id) {
+						return false;
+					}
 				}
-			}
+				for (let j = 0; j < reviewDeck.seeds.length; j++) {
+					if (reviewDeck.seeds[j] === seed.id) {
+						return false;
+					}
+				}
+				return true;
+			});
+
+			// Add missing everyday seeds from the pool
+			const missingEveryday = seedsPool.filter((seed) => seed.showEveryday).map((seed) => seed.id);
+			reviewDeck.seeds = reviewDeck.seeds.concat(missingEveryday);
+
+			// Remove everyday seeds from the pool
+			seedsPool = seedsPool.filter((seed) => !seed.showEveryday);
+			// Convert pool to array of IDs
+			seedsPool = seedsPool.map((seed) => seed.id);
+
+			// Get everyday seeds in review deck
 			for (let j = 0; j < reviewDeck.seeds.length; j++) {
-				if (reviewDeck.seeds[j] === seed.id) {
-					return false;
+				const seedID = reviewDeck.seeds[j];
+				const isEveryday = appDeck.seeds.find((seed) => seed.id === seedID && seed.showEveryday);
+				if (isEveryday) {
+					everydaySeeds.push(seedID);
 				}
 			}
-			return true;
-		});
 
-		// Add missing everyday seeds from the pool
-		const missingEveryday = seedsPool.filter((seed) => seed.showEveryday).map((seed) => seed.id);
-		reviewDeck.seeds = reviewDeck.seeds.concat(missingEveryday);
+			// Calculate deck limit
+			const inReviewCount = reviewDeck.seeds.length - everydaySeeds.length;
+			const missingCount = limit - reviewedCount - inReviewCount;
+			let target = limit - reviewedCount;
 
-		// Remove everyday seeds from the pool
-		seedsPool = seedsPool.filter((seed) => !seed.showEveryday);
-		// Convert pool to array of IDs
-		seedsPool = seedsPool.map((seed) => seed.id);
+			// Add missing seeds
+			if (missingCount > 0) {
+				// Constrain limit
+				if (target > inReviewCount + seedsPool.length) {
+					target = inReviewCount + seedsPool.length;
+				}
 
-		// Get everyday seeds count in review deck
-		for (let j = 0; j < reviewDeck.seeds.length; j++) {
-			const seedID = reviewDeck.seeds[j];
-			const isEveryday = appDeck.seeds.find((seed) => seed.id === seedID && seed.showEveryday);
-			if (isEveryday) {
-				everydaySeeds.push(seedID);
+				while (reviewDeck.seeds.length - everydaySeeds.length < target) {
+					const randomIndex = Math.floor(Math.random() * seedsPool.length);
+					reviewDeck.seeds.push(seedsPool[randomIndex]);
+					seedsPool.splice(randomIndex, 1);
+				}
 			}
-		}
+			// Remove seeds above limit
+			else if (missingCount < 0) {
+				if (target < 0) {
+					target = 0;
+				}
 
-		// Calculate deck limit
-		const inReviewCount = reviewDeck.seeds.length - everydaySeeds.length;
-		const missingCount = limit - reviewedCount - inReviewCount;
-		let target = limit - reviewedCount;
-
-		// Add missing seeds
-		if (missingCount > 0) {
-			// Constrain limit
-			if (target > inReviewCount + seedsPool.length) {
-				target = inReviewCount + seedsPool.length;
-			}
-
-			while (reviewDeck.seeds.length - everydaySeeds.length < target) {
-				const randomIndex = Math.floor(Math.random() * seedsPool.length);
-				reviewDeck.seeds.push(seedsPool[randomIndex]);
-				reviewDeck.seeds = uniq(reviewDeck.seeds);
-			}
-		}
-
-		// Remove seeds above limit
-		else if (missingCount < 0) {
-			if (target < 0) {
-				target = 0;
-			}
-
-			while (reviewDeck.seeds.length - everydaySeeds.length > target) {
-				const randomIndex = Math.floor(Math.random() * reviewDeck.seeds.length);
-				// Don't remove everyday seeds
-				if (!everydaySeeds.find((seed) => seed === reviewDeck.seeds[randomIndex])) {
-					reviewDeck.seeds.splice(randomIndex, 1);
+				while (reviewDeck.seeds.length - everydaySeeds.length > target) {
+					const randomIndex = Math.floor(Math.random() * reviewDeck.seeds.length);
+					// Don't remove everyday seeds
+					if (!everydaySeeds.find((seed) => seed === reviewDeck.seeds[randomIndex])) {
+						reviewDeck.seeds.splice(randomIndex, 1);
+					}
 				}
 			}
 		}
