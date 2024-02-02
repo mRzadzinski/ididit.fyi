4964b31d982b40f46b1c0d661b144c19604a2081
Maciej Rzadzinski <maciej.rzd@gmail.com>
2024-02-02 17:57:07 +0100

    Position SearchBtn

---
+++ src/components/common/SearchBtn.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import SearchIcon from '~icons/material-symbols/search';
+
+	export let clickHandler: Function;
+</script>
+
+<ul class="menu min-w-fit flex p-0 mb-[0.15rem]">
+	<li class="flex items-center">
+		<div
+			class="p-0 flex justify-center items-center rounded-full cursor-default"
+			role="button"
+			tabindex="0"
+			on:click={(e) => {
+				e.stopImmediatePropagation();
+				clickHandler();
+			}}
+			on:keydown={() => {
+				clickHandler();
+			}}
+		>
+			<div class="dropdown dropdown-top dropdown-end rounded-full w-5 h-5">
+				<label
+					class="w-full h-full flex items-center justify-center z-[0]"
+					tabindex="-1"
+					for="search"
+				>
+					<SearchIcon style="font-size: 0.77rem; margin-right: 0.05rem;" />
+				</label>
+			</div>
+		</div>
+	</li>
+</ul>
